9adfc85e7861fd7dfb9531a2d52d8687be5ff137
ENERJ\Filippo <filippo.erbisti@enerj.it>
2023-01-18 17:48:59 +0100

    some features

--- scripts/script.js
+++ scripts/script.js
@@ -12,72 +12,72 @@ new Vue({
         {
           name: "Mekanın Sahibi",
           artist: "Norm Ender",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
+          cover: "./img/1.jpg",
+          source: "./mp3/1.mp3",
           url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
           favorited: false
         },
         {
           name: "Everybody Knows",
           artist: "Leonard Cohen",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          cover: "./img/2.jpg",
+          source: "./mp3/2.mp3",
           url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
           favorited: true
         },
         {
           name: "Extreme Ways",
           artist: "Moby",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+          cover: "./img/3.jpg",
+          source: "./mp3/3.mp3",
           url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
           favorited: false
         },
         {
           name: "Butterflies",
           artist: "Sia",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+          cover: "./img/4.jpg",
+          source: "./mp3/4.mp3",
           url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
           favorited: false
         },
         {
           name: "The Final Victory",
           artist: "Haggard",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+          cover: "./img/5.jpg",
+          source: "./mp3/5.mp3",
           url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
           favorited: true
         },
         {
           name: "Genius ft. Sia, Diplo, Labrinth",
           artist: "LSD",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+          cover: "./img/6.jpg",
+          source: "./mp3/6.mp3",
           url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
           favorited: false
         },
         {
           name: "The Comeback Kid",
           artist: "Lindi Ortega",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+          cover: "./img/7.jpg",
+          source: "./mp3/7.mp3",
           url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
           favorited: true
         },
         {
           name: "Overdose",
           artist: "Grandson",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+          cover: "./img/8.jpg",
+          source: "./mp3/8.mp3",
           url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
           favorited: false
         },
         {
           name: "Rag'n'Bone Man",
           artist: "Human",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+          cover: "./img/9.jpg",
+          source: "./mp3/9.mp3",
           url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
           favorited: false
         }
@@ -97,6 +97,7 @@ new Vue({
         this.isTimerPlaying = false;
       }
     },
+    
     generateTime() {
       let width = (100 / this.audio.duration) * this.audio.currentTime;
       this.barWidth = width + "%";
@@ -120,6 +121,7 @@ new Vue({
       this.duration = durmin + ":" + dursec;
       this.currentTime = curmin + ":" + cursec;
     },
+
     updateBar(x) {
       let progress = this.$refs.progress;
       let maxduration = this.audio.duration;
@@ -136,11 +138,13 @@ new Vue({
       this.audio.currentTime = (maxduration * percentage) / 100;
       this.audio.play();
     },
+
     clickProgress(e) {
       this.isTimerPlaying = true;
       this.audio.pause();
       this.updateBar(e.pageX);
     },
+
     prevTrack() {
       this.transitionName = "scale-in";
       this.isShowCover = false;
@@ -152,6 +156,7 @@ new Vue({
       this.currentTrack = this.tracks[this.currentTrackIndex];
       this.resetPlayer();
     },
+
     nextTrack() {
       this.transitionName = "scale-out";
       this.isShowCover = false;
@@ -163,6 +168,7 @@ new Vue({
       this.currentTrack = this.tracks[this.currentTrackIndex];
       this.resetPlayer();
     },
+
     resetPlayer() {
       this.barWidth = 0;
       this.circleLeft = 0;
@@ -176,12 +182,14 @@ new Vue({
         }
       }, 300);
     },
+
     favorite() {
       this.tracks[this.currentTrackIndex].favorited = !this.tracks[
         this.currentTrackIndex
       ].favorited;
     }
   },
+
   created() {
     let vm = this;
     this.currentTrack = this.tracks[0];
